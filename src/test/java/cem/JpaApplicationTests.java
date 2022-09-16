0cc410a7c52b22b1ccc4f810f59c68dc6da22305
cemakay <cema0092@stud.kea.dk>
2022-09-16 15:38:53 +0200

    model package

--- src/test/java/cem/JpaApplicationTests.java
+++ src/test/java/cem/JpaApplicationTests.java
@@ -3,11 +3,11 @@ package cem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/*@SpringBootTest
 class JpaApplicationTests {
 
     @Test
     void contextLoads() {
     }
 
-}
+}*/
