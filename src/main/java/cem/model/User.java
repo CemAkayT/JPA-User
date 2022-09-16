e5ba74f228d6c316d012e21ad3229f736539105b
cemakay <cema0092@stud.kea.dk>
2022-09-16 17:32:17 +0200

    connection test

--- src/main/java/cem/model/User.java
+++ src/main/java/cem/model/User.java
@@ -1,6 +1,17 @@
 package cem.model;
 
+import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Data // laver getter, setter, toString(), hashCode()
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
